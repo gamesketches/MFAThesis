8713b631a6e0f6a675b75732126071b296b73209
gamesketches <sam.von.ehren@gmail.com>
2016-09-06 22:06:32 -0400

    got a background and a bit of movement

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	Rigidbody2D playerRB;
+	Vector3 lastFramePosition;
+	// Use this for initialization
+	void Start () {
+		playerRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+		lastFramePosition = playerRB.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+		position.x += playerRB.position.x - lastFramePosition.x;
+		transform.position = position;
+		lastFramePosition = playerRB.position;
+	}
+}
---
+++ Assets/MarioMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarioMovement : MonoBehaviour {
+
+	Animator animationController;
+	Rigidbody2D rb;
+	SpriteRenderer renderer;
+	float distanceTraveled;
+	bool turnTheBeatAround;
+	// Use this for initialization
+	void Start () {
+		animationController = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
+		distanceTraveled = 0;
+		turnTheBeatAround = false;
+		renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis("Horizontal");
+		float jump = Input.GetAxis("Jump");
+		if(rb.velocity.x > 5) {
+			rb.AddForce(new Vector2(hori, jump), ForceMode2D.Impulse);
+		}
+		animationController.SetInteger("movement", Mathf.CeilToInt(hori));
+		animationController.SetBool("grounded", jump == 0);
+		distanceTraveled += hori;
+		if(distanceTraveled > 300) {
+			turnTheBeatAround = true;
+		}
+	 	renderer.flipX = FlipSprite();
+	}
+
+	bool FlipSprite(){
+		if(turnTheBeatAround) {
+			return rb.velocity.x > 0 ? false : true;
+		}
+		else {
+			return rb.velocity.x > 0 ? true : false;
+		}
+	}
+}
