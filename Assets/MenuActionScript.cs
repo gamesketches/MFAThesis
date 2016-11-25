c81c07d0061ef313da6f4417be8152ebfefcc493
gamesketches <sam.von.ehren@gmail.com>
2016-11-25 14:30:43 -0500

    Last preparations for making this a fully dynamic system, just need to wire up mouse control and test

---
+++ Assets/MenuActionScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Reflection;
+
+public class MenuActionScript : MonoBehaviour {
+
+	private MethodInfo action;
+	private object actionObject;
+	private object[] parameters;
+	// Use this for initialization
+	void Start () {
+		parameters = new object[0];
+	}
+	
+	void Initialize(MethodInfo method, object obj) {
+		action = method;
+		actionObject = obj;
+	}
+
+	void Initialize(MethodInfo method, object obj, object[] methodParameters) {
+		action = method;
+		actionObject = obj;
+		parameters = methodParameters;
+	}
+
+	void Activate() {
+		action.Invoke(actionObject, parameters);
+	}
+}
