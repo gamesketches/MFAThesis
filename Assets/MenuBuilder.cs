fb5410daede4838e3013f1cfd5ed6f5c1f76588a
gamesketches <sam.von.ehren@gmail.com>
2016-11-29 17:24:27 -0500

    Menus now propagating mostly correctly

--- Assets/MenuBuilder.cs
+++ Assets/MenuBuilder.cs
@@ -17,6 +17,7 @@ public class MenuBuilder : MonoBehaviour {
 	void Start () {
 		RectTransform rect = Resources.Load<GameObject>("prefabs/MenuAction").GetComponent<RectTransform>();
 		offset = new Vector3(rect.rect.width, rect.rect.height, 0);
+		Debug.Log(offset);
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<MenuBasedPlatformerMovement>();
 		Debug.Log(player);
 		TextAsset menuData = Resources.Load("MenuXML") as TextAsset;
@@ -27,18 +28,16 @@ public class MenuBuilder : MonoBehaviour {
 
 		XmlNode topNode = menuXML.ChildNodes[1].FirstChild;
 
-		GenMenuList(topNode, transform, new Vector3(0, 0, 0));
-
-		//MethodInfo method = playerType.GetMethod(topNode.FirstChild.InnerText);
-		//method.Invoke(player, new object[0]);
+		GenMenuList(topNode, transform, new Vector3(-offset.x, 0, 0));
 	}
 	
 	// Update is called once per frame
 	void GenMenuList (XmlNode topNode, Transform parent, Vector3 position) {
 		Vector3 newPos = position;
 		newPos.x += offset.x;
+		Debug.Log(newPos);
 		GameObject listOption = (GameObject)Instantiate(Resources.Load<GameObject>("prefabs/OptionList"), newPos, Quaternion.identity);
-		listOption.transform.parent = transform;
+		listOption.transform.SetParent(parent, false);
 		int numOptionsInList = 0;
 		foreach(XmlNode node in topNode.ChildNodes) {
 			if(node.Name == "Action") {
