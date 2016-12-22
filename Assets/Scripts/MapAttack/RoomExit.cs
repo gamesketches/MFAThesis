adc85eb9cb9c445705fc59967eb5f50eb330db40
gamesketches <sam.von.ehren@gmail.com>
2016-12-22 18:40:34 -0500

    first pass on implementing zoom in

--- Assets/Scripts/MapAttack/RoomExit.cs
+++ Assets/Scripts/MapAttack/RoomExit.cs
@@ -24,7 +24,50 @@ public class RoomExit : MonoBehaviour {
 				return;
 			} 
 		}
-		other.transform.position = Vector3.zero;
-		other.GetComponent<MappedController>().RemappControls(dir);
+		StartCoroutine(RoomTransition(other.gameObject));
+	}
+
+	IEnumerator RoomTransition(GameObject player) {
+		float t = 0;
+		Vector3 targetPos = transform.position;
+		Vector3 startPos = Camera.main.transform.position;
+		float rotationAmount = 0;
+		switch(dir) {
+			case Direction.Up:
+				rotationAmount = 0;
+				break;
+			case Direction.Left:
+				rotationAmount = 90;
+				break;
+			case Direction.Right:
+				rotationAmount = -90;
+				break;
+			case Direction.Down:
+				rotationAmount = 180;
+				break;
+			default:
+				Debug.LogError("Unknown Directional Enum");
+				break;
+		}
+		Quaternion targetRotation = Quaternion.Euler(0, 0, rotationAmount);
+		while(t < 1) {
+			Camera.main.transform.position = Vector3.Lerp(startPos, targetPos, t);
+			Camera.main.transform.rotation = Quaternion.Lerp(Quaternion.identity, targetRotation, t);
+			Camera.main.fieldOfView = Mathf.Lerp(60, 1, t);
+			t += Time.deltaTime;
+			yield return null;
+		}
+
+		Camera.main.transform.position = new Vector3(0, 1, -10);
+		Camera.main.transform.rotation = Quaternion.identity;
+		Camera.main.fieldOfView = 60;
+		startPos = new Vector3(0f, -3.5f, 0f);
+		t = 0;
+		while(t < 0.5) {
+			player.transform.position = Vector3.Lerp(startPos, Vector3.zero, t);
+			t += Time.deltaTime;
+			yield return null;
+		}
+		player.GetComponent<MappedController>().RemappControls(dir);
 	}
 }
