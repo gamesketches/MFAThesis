9233684d4a39ab058019b8bf9c9305279f7c737d
gamesketches <sam.von.ehren@gmail.com>
2017-01-10 14:33:16 -0500

    testing out different options for one dimensional blocks

--- Assets/DirectionalBlockBehavior.cs
+++ Assets/DirectionalBlockBehavior.cs
@@ -8,21 +8,38 @@ public class DirectionalBlockBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		SetConstraints();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if(other.gameObject.tag == "directionBlock") {
+			rb.constraints = RigidbodyConstraints2D.None;
+			Debug.Log("removing constraints");
+			Debug.Log(other.contacts[0].point.y < transform.position.y);
+		}
+		else {
+			Debug.Log(other.gameObject.tag);
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D other) {
+		if(other.gameObject.tag == "directionBlock") {
+			SetConstraints();
+			Debug.Log("reenabling constraints");	
+		}
+	}
+
+	void SetConstraints(){
 		if(direction == Direction.Up || direction == Direction.Down) {
 			rb.constraints = RigidbodyConstraints2D.FreezePositionX;
 		}
 		else {
 			rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector2 curVelocity = rb.velocity;
-		if(direction == Direction.Up) {
-			if(curVelocity.y < 0) {
-				rb.velocity = Vector2.zero;
-			}
 		}
 	}
 }
