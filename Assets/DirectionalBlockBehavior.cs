e9b1df4a9f2d9d07bb0818edb58ebe69f7ff4408
gamesketches <sam.von.ehren@gmail.com>
2017-01-26 12:47:56 -0500

    Got directional block pushing working, changed colors of block pushing

--- Assets/DirectionalBlockBehavior.cs
+++ Assets/DirectionalBlockBehavior.cs
@@ -5,18 +5,46 @@ public class DirectionalBlockBehavior : MonoBehaviour {
 
 	public Direction direction;
 	Rigidbody2D rb;
+	DirectionalBlockCollider[] colliders;
+	Vector2[] directionVector = {new Vector2(0, 1),
+								new Vector2 (0, -1),
+								new Vector2 (-1, 0),
+								 new Vector2(1, 0)};
+	bool moving;
 	// Use this for initialization
 	void Start () {
+		moving = false;
+		colliders = GetComponentsInChildren<DirectionalBlockCollider>();
+		colliders[(int)direction].coreDirection = true;
 		rb = GetComponent<Rigidbody2D>();
-		SetConstraints();
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		switch(direction) {
+			case Direction.Up:
+				renderer.color = Color.blue;
+				break;
+			case Direction.Down:
+				renderer.color = Color.red;
+				break;
+			case Direction.Left:
+				renderer.color = Color.white;
+				break;
+			case Direction.Right:
+				renderer.color = Color.green;
+				break;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		foreach(DirectionalBlockCollider collider in colliders) {
+			if(!moving && collider.colliding) {
+			StartCoroutine(Move(directionVector[(int)direction]));
+		}
 
+		}
 	}
 
-	void OnCollisionEnter2D(Collision2D other) {
+	/*void OnCollisionEnter2D(Collision2D other) {
 		if(other.gameObject.tag == "directionBlock") {
 			rb.constraints = RigidbodyConstraints2D.None;
 			Debug.Log("removing constraints");
@@ -32,7 +60,7 @@ public class DirectionalBlockBehavior : MonoBehaviour {
 			SetConstraints();
 			Debug.Log("reenabling constraints");	
 		}
-	}
+	}*/
 
 	void SetConstraints(){
 		if(direction == Direction.Up || direction == Direction.Down) {
@@ -42,4 +70,21 @@ public class DirectionalBlockBehavior : MonoBehaviour {
 			rb.constraints = RigidbodyConstraints2D.FreezePositionY;
 		}
 	}
+
+	IEnumerator Move(Vector2 directionVector) {
+		moving = true;
+		Vector3 movementVector = new Vector3(directionVector.x, directionVector.y, 0);
+		Vector3 startPos = transform.position;
+		Vector3 endPos = startPos + movementVector;
+		float t = 0;
+		while(t < 1) {
+			transform.position = Vector3.Lerp(startPos, endPos, t);
+			t += Time.deltaTime;
+			yield return null;
+		}
+
+		colliders[(int)direction].colliding = false;
+		moving = false;
+	}
+
 }
---
+++ Assets/DirectionalBlockCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionalBlockCollider : MonoBehaviour {
+
+	public bool colliding;
+	public bool coreDirection;
+	// Use this for initialization
+	void Awake () {
+		colliding = false;
+		coreDirection = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player" && coreDirection){
+			colliding = true;
+			}
+		else if(other.tag == "directionBlock") {		
+			colliding = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		colliding = false;
+	}
+}
