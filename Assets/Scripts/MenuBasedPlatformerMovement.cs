85e3b07444bceb9736fd1e7c225b50544334cb05
gamesketches <sam.von.ehren@gmail.com>
2017-02-02 14:15:21 -0500

    Tons of new verbs for menu based platformer

--- Assets/Scripts/MenuBasedPlatformerMovement.cs
+++ Assets/Scripts/MenuBasedPlatformerMovement.cs
@@ -10,6 +10,7 @@ public class MenuBasedPlatformerMovement : MonoBehaviour {
 	public float speed;
 	private float movementSpeed;
 	private int direction;
+	private bool doubleJumped;
 	// Use this for initialization
 	void Start () {
 		direction = 1;
@@ -25,6 +26,7 @@ public class MenuBasedPlatformerMovement : MonoBehaviour {
 		if(!animator.GetBool("grounded")) {
 			if(rb.velocity.y == 0) {
 				animator.SetBool("grounded", true);
+				doubleJumped = false;
 			}
 		}
 	}
@@ -47,6 +49,28 @@ public class MenuBasedPlatformerMovement : MonoBehaviour {
 		movementSpeed = 0;
 	}
 
+	public void Slide() {
+		movementSpeed = 0;
+		transform.rotation = Quaternion.Euler(0, 0, 90);
+		Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0);
+		StartCoroutine(Sliding());
+	}
+
+	public void Dash() {
+		rb.AddForce(new Vector2(6, 1), ForceMode2D.Impulse);
+	}
+
+	IEnumerator Sliding() {
+		float t = 0;
+		while(t < 0.5f) {
+			transform.Translate(0, -4 * Time.deltaTime * direction, 0);
+			t += Time.deltaTime;
+			yield return null;
+		}
+		transform.rotation = Quaternion.identity;
+		Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0);
+	}
+
 	public void Jump() {
 		if(animator.GetBool("grounded")) {
 			animator.SetBool("grounded", false);
@@ -57,8 +81,77 @@ public class MenuBasedPlatformerMovement : MonoBehaviour {
 	public void Hop() {
 		if(animator.GetBool("grounded")) {
 			animator.SetBool("grounded", false);
-			rb.AddForce(new Vector2(0, jumpPower / 2), ForceMode2D.Impulse);
+			rb.AddForce(new Vector2(2 * direction, jumpPower / 2), ForceMode2D.Impulse);
 		}
+	}
+	public void SuperJump() {
+		if(animator.GetBool("grounded")) {
+			animator.SetBool("grounded", false);
+			rb.AddForce(new Vector2(0, jumpPower * 1.7f), ForceMode2D.Impulse);
+		}
+	}
+
+	public void DoubleJump() {
+		if(!animator.GetBool("grounded") && !doubleJumped) {
+			doubleJumped = true;
+			rb.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+		}
+	}
+
+	public void AirDash() {
+		if(!animator.GetBool("grounded") && !doubleJumped) {
+			doubleJumped = true;
+			Debug.Log("dashin");
+			rb.AddForce(new Vector2(4, 1), ForceMode2D.Impulse);
+		}
+	}
+
+	public void SlideAttack() {
+		Middle();
+		Slide();
+	}
+
+	public void StudderStep() {
+		StartCoroutine(DoStudderStep());
+	}
+
+	IEnumerator DoStudderStep() {
+		if(animator.GetBool("grounded")) {
+			animator.SetBool("grounded", false);
+			rb.AddForce(new Vector2(3 * -direction, jumpPower / 3), ForceMode2D.Impulse);
+			while(!animator.GetBool("grounded")) {
+				yield return null;
+			}
+			Thrust();
+		}
+	}
+
+	public void Thrust() {
+		Middle();
+		Dash();
+	}
+
+	public void JumpSlash() {
+		if(animator.GetBool("grounded")) {
+			animator.SetBool("grounded", false);
+			Low();
+			ApplyMovementForces(5 * direction, jumpPower);
+		}
+	}
+
+	public void SpinAttack() {
+		JumpSlash();
+		StartCoroutine(SpinPlayer());
+	}
+
+	IEnumerator SpinPlayer() {
+		while(!animator.GetBool("grounded")) {
+			transform.Rotate(Vector3.forward * 5);
+			Camera.main.transform.rotation = Quaternion.identity;
+			yield return null;
+		}
+		transform.rotation = Quaternion.identity;
+		Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0);
 	}
 
 	public void High() {
@@ -83,6 +176,7 @@ public class MenuBasedPlatformerMovement : MonoBehaviour {
 
 
 	IEnumerator ManageAttackHitBox(GameObject attack) {
+		attack.transform.SetParent(transform);
 		yield return new WaitForSeconds(1);
 		Destroy(attack);
 
@@ -91,5 +185,9 @@ public class MenuBasedPlatformerMovement : MonoBehaviour {
 
 	public void PrintTest() {
 		Debug.Log("The test worked");
+	}
+
+	void ApplyMovementForces(float x, float y) {
+		rb.AddForce(new Vector2(x, y), ForceMode2D.Impulse);
 	}
 }
