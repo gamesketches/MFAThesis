bbacb76ae8336a457b0951143be1612bae3a5f96
gamesketches <sam.von.ehren@gmail.com>
2017-04-18 15:23:05 -0400

    Added some actual enemy movement behavior

--- Assets/Scripts/MenuPlatformerEnemy.cs
+++ Assets/Scripts/MenuPlatformerEnemy.cs
@@ -8,21 +8,38 @@ public class MenuPlatformerEnemy : MonoBehaviour {
 
 	public EnemyType myType;
 
+	delegate void UpdateFunction();
+
+	public float movementRange;
+	public float airSpeed;
+
+	UpdateFunction myUpdate;
+
+	Vector3 startPos;
 	// Use this for initialization
 	void Start () {
-		if(myType == EnemyType.Air) {
+		switch(myType) {
+		case EnemyType.Air:
 			GetComponent<Rigidbody2D>().gravityScale = 0;
+			myUpdate = AirUpdate;
+			break;
+		case EnemyType.Mid:
+			myUpdate = MidUpdate;
+			break;
+		default:
+			myUpdate = GroundUpdate;
+			break;
 		}
+		startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		myUpdate();
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag == "Player") {
-			Debug.Log("wat");
 			if(!other.gameObject.GetComponent<MenuBasedPlatformerMovement>().blocking) {
 				SceneManager.LoadScene(0);	
 			}
@@ -30,5 +47,17 @@ public class MenuPlatformerEnemy : MonoBehaviour {
 		else {
 			Debug.Log(other.gameObject.name);
 		}
+	}
+
+	void GroundUpdate() {
+		transform.Translate(Mathf.PingPong(Time.time, movementRange) - movementRange / 2, 0, 0);
+	}
+
+	void MidUpdate() {
+
+	}
+
+	void AirUpdate() {
+		transform.Translate((Mathf.Sin(Time.time) * movementRange), Mathf.Sin(Time.time * airSpeed) * movementRange, 0);
 	}
 }
