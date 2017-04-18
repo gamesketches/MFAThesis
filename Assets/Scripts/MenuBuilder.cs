33a67c987b81cac4e6b3f01b4bae88e956c8818a
gamesketches <sam.von.ehren@gmail.com>
2017-04-18 15:53:24 -0400

    Menu Builder is now completely modular

--- Assets/Scripts/MenuBuilder.cs
+++ Assets/Scripts/MenuBuilder.cs
@@ -14,25 +14,26 @@ public class MenuBuilder : MonoBehaviour {
 	MenuBasedPlatformerMovement player;
 	Type playerType;
 	MenuController pointer;
+	public string menuSource;
+	public string targetObjectType;
+	public GameObject listOptionPrefab;
+	public GameObject menuActionPrefab;
 	public Vector3 startPosition;
-	public playerClass myClass;
 	public Vector3 listEntryOffset;
 	public Vector3 subListOffset;
 
 	// Use this for initialization
 	void Start () {
-		RectTransform rect = Resources.Load<GameObject>("prefabs/MenuAction").GetComponent<RectTransform>();
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<MenuBasedPlatformerMovement>();
-		TextAsset menuData = Resources.Load("MenuXML" + myClass.ToString()) as TextAsset;
+		TextAsset menuData = Resources.Load(menuSource) as TextAsset;
 
-		playerType = Type.GetType("MenuBasedPlatformerMovement");
+		playerType = Type.GetType(targetObjectType);
 		menuXML = new XmlDocument();
 		menuXML.LoadXml(menuData.text);
 
 		XmlNode topNode = menuXML.ChildNodes[1].FirstChild;
 
 		pointer = GetComponent<MenuController>();
-
 
 		pointer.Initialize(GenMenuList(topNode, transform, startPosition));
 
@@ -41,20 +42,20 @@ public class MenuBuilder : MonoBehaviour {
 	// Update is called once per frame
 	Transform GenMenuList (XmlNode topNode, Transform parent, Vector3 position) {
 		Vector3 newPos = position;
-		GameObject listOption = (GameObject)Instantiate(Resources.Load<GameObject>("prefabs/OptionList"), newPos, Quaternion.identity);
+		GameObject listOption = (GameObject)Instantiate(listOptionPrefab, newPos, Quaternion.identity);
 		listOption.transform.SetParent(parent, false);
 		int numOptionsInList = 0;
 		foreach(XmlNode node in topNode.ChildNodes) {
 			if(node.Name == "Action") {
-				GameObject actionOption = (GameObject)Instantiate(Resources.Load<GameObject>("prefabs/MenuAction"), newPos, Quaternion.identity);
+				GameObject actionOption = (GameObject)Instantiate(menuActionPrefab, newPos, Quaternion.identity);
 				actionOption.transform.SetParent(listOption.transform, false);
 				actionOption.GetComponent<MenuActionScript>().Initialize(playerType.GetMethod(node.InnerText), player);
 				actionOption.GetComponentInChildren<Text>().text = node.InnerText;
 			}
 			else if(node.Name == "List") {
-				GameObject actionOption = (GameObject)Instantiate(Resources.Load<GameObject>("prefabs/MenuAction"), newPos, Quaternion.identity);
+				GameObject actionOption = (GameObject)Instantiate(menuActionPrefab, newPos, Quaternion.identity);
 				actionOption.transform.SetParent(listOption.transform, false);
-				actionOption.GetComponent<MenuActionScript>().InitializeAsList(GenMenuList(node, actionOption.transform, subListOffset));//childPos));
+				actionOption.GetComponent<MenuActionScript>().InitializeAsList(GenMenuList(node, actionOption.transform, subListOffset));
 				actionOption.GetComponentInChildren<Text>().text = node.Attributes[0].Value;
 			}
 			else Debug.Log(node.InnerText);
